dbf04157a6304be8c4e8942f979bbec40dc26dfc
LuckyHipster958 <oscarnegro479@gmail.com>
2025-03-19 11:42:23 +0100

    Update aniadir.c

--- aniadir.c
+++ aniadir.c
@@ -1,4 +1,10 @@
-int fd = open("archivo.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-dup2(fd, STDOUT_FILENO);
-close(fd);
-execvp(args[0], args);
+if (strcmp(args[i], ">>") == 0) {
+    int fd = open(args[i + 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (fd == -1) {
+        perror("Error abriendo archivo");
+        return 1;
+    }
+    dup2(fd, STDOUT_FILENO);
+    close(fd);
+    args[i] = NULL; // Eliminar ">>" y el nombre del archivo de args
+}
